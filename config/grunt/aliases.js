c698e481ceb049e3ad96de4af8834b49262912c3
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-05-02 22:13:27 +0200

    build(config): configure tooling for TypeSript

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,10 +1,16 @@
 module.exports = {
+    build: [
+        'clean:build',
+        'sh:build',
+        'babel:build'
+    ],
     lint: [
         'sh:lint-config',
         'sh:lint-src',
         'sh:lint-test'
     ],
     test: [
+        'build',
         'sh:test-unit'
     ]
 };
--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -2,11 +2,14 @@ module.exports = (grunt) => {
     const fix = (grunt.option('fix') === true);
 
     return {
+        'build': {
+            cmd: 'tsc --project src/tsconfig.json'
+        },
         'lint-config': {
             cmd: `eslint --config config/eslint/config.json --ext .js ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives *.js config/`
         },
         'lint-src': {
-            cmd: `eslint --config config/eslint/src.json --ext .js ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives src/`
+            cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
         },
         'lint-test': {
             cmd: `eslint --config config/eslint/test.json --ext .js ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives test/`
