2563c6e8883dbee923bbe72565408ab3fbeb922a
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-02-25 17:15:57 +0100

    fix: support predefined interfaces
    Previously dataToItem() only worked with interfered types but not with previously defined
    interfaces. This should be fixed now.
    
    fix #82

--- src/types/derived-item-object.ts
+++ src/types/derived-item-object.ts
@@ -1,4 +1,8 @@
-import { IDataObject } from '../interfaces';
+import { TDataValue } from './data-value';
 import { TDerivedItemValue } from './derived-item-value';
 
-export type TDerivedItemObject<T> = T extends IDataObject ? { [U in keyof T]: TDerivedItemValue<T[U]> } : never;
+export type TDerivedItemObject<T> = keyof T extends string
+    ? T[keyof T] extends TDataValue
+        ? { [U in keyof T]: TDerivedItemValue<T[U]> }
+        : never
+    : never;
