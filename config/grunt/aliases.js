bec16f2f3b12fee21cdccf7d738964d8068218c7
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-06-01 00:52:54 +0200

    build(config): use rimraf to delete build directory

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -1,5 +1,5 @@
 module.exports = {
-    build: ['clean:build', 'sh:build'],
+    build: ['sh:clean', 'sh:build'],
     lint: ['sh:lint-config', 'sh:lint-src', 'sh:lint-test'],
     test: ['build', 'sh:test-unit']
 };
--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -6,6 +6,9 @@ module.exports = (grunt) => {
             cmd: `tsc --project src/tsconfig.json && \
                 babel ./build/es2019 --config-file ./config/babel/build.json --out-dir ./build/node`
         },
+        'clean': {
+            cmd: 'rimraf build/*'
+        },
         'lint-config': {
             cmd: `eslint --config config/eslint/config.json --ext .js ${fix ? '--fix ' : ''}--report-unused-disable-directives *.js config/`
         },
