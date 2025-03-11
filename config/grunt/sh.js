5f982ccd719fe4d40e99239c211fd5352c8b99be
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-11 13:57:00 +0100

    build(config): remove linting tasks

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -3,15 +3,6 @@ module.exports = () => {
         'build': {
             cmd: 'npm run build'
         },
-        'lint-config': {
-            cmd: 'npm run lint:config'
-        },
-        'lint-src': {
-            cmd: 'npm run lint:src'
-        },
-        'lint-test': {
-            cmd: 'npm run lint:test'
-        },
         'test-unit': {
             cmd: 'mocha --bail --parallel --recursive --require config/mocha/config-unit.js test/unit'
         }
